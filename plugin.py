bbdbc54efa0ca297c8dbc5f3ed6e9c9fcd4cac80
noname009 <48664297+noname009@users.noreply.github.com>
2019-10-01 16:48:42 +0900

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -35,7 +35,7 @@ def plugin_unload():
     Logic.plugin_unload()
 
 plugin_info = {
-    'version' : '0.1.0.4',
+    'version' : '0.1.0.5',
     'name' : 'Manamoa 다운로더',
     'category_name' : 'service',
     'icon' : '',
