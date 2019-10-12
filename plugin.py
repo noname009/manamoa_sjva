b3a3932ffda24d7a02d2f9376d688d5dd006b271
noanme <noname@bqbro.com>
2019-10-12 19:29:36 +0900

    ver up

--- plugin.py
+++ plugin.py
@@ -35,7 +35,7 @@ def plugin_unload():
     Logic.plugin_unload()
 
 plugin_info = {
-    'version' : '0.1.1.1',
+    'version' : '0.1.1.2',
     'name' : 'Manamoa 다운로더',
     'category_name' : 'service',
     'icon' : '',
